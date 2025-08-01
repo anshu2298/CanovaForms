321f8db9a54eddfecc4b303cdc6d80c0a4946d46
anshu2298 <amshusingh2298@gmail.com>
2025-08-01 23:38:18 +0530

    minor changes

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 
 // const allowedOrigins = ["http://localhost:5173"];
 const allowedOrigins = [
-  "https://canovaforms-frontend.onrender.com/",
+  "https://canovaforms-frontend.onrender.com",
 ];
 app.use(express.json());
 app.use(cookieParser());
